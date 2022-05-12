39aee87593aaef7147e767ebc86accb2c0526cc1
Dakesh <daulet.tusupbaev@gmail.com>
2022-05-12 06:30:47 +0600

    Detail news article fragment created

--- app/src/main/java/com/example/news/fragments/ArticlesFragment.kt
+++ app/src/main/java/com/example/news/fragments/ArticlesFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.news.R
+import com.example.news.databinding.FragmentArticlesBinding
+import com.example.news.viewmodel.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_articles.*
 
 class ArticlesFragment : Fragment() {
+
+    private val viewModel: NewsViewModel by viewModels()
+    val args: ArticlesFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +25,19 @@ class ArticlesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_articles, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val article = args.article
+
+        webView.apply {
+            webViewClient = object : WebViewClient(){
+                override fun onPageCommitVisible(view: WebView?, url: String?) {
+                    super.onPageCommitVisible(view, url)
+                    progressBar.visibility = View.GONE
+                }
+            }
+            loadUrl(article.url.toString())
+        }
     }
 }
